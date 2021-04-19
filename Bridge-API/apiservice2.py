0c6c0cce380edbb0f96b5fbfd22f0bddb063d9ea
CristianValero <pauito03@hotmail.com>
2021-04-19 10:38:23 +0200

    token access work correctly

--- Bridge-API/apiservice2.py
+++ Bridge-API/apiservice2.py
@@ -6,12 +6,17 @@ import uuid
 import jwt
 import datetime
 import os
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Th1s1ss3cr3t'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/cristian/Desktop/AgataBot-v2/AgataBot/Bridge-API/authapi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
+API_TRANSLATE_TO_ES = 'http://0.0.0.0:3002/translator/es/'
+API_TRANSLATE_TO_EN = 'http://0.0.0.0:3002/translator/en/'
+API_OBTAIN_ANSWER = 'http://0.0.0.0:3001/agata/answer/'
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,8 +34,9 @@ def token_required(f):
             return jsonify({
                 'message': 'A valid token is missing.'
             })
+            
         try:
-            data = jwt.decode(token, app.config[SECRET_KEY])
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = Users.query.filter_by(public_id=data['public_id']).first()
         except:
             return jsonify({
@@ -40,6 +46,11 @@ def token_required(f):
         return f(current_user, *args, **kwargs)
     return decorator
 
+def throw_request_error():
+    return jsonify({
+        'message': 'The intern request not be valid.'
+    })
+
 @app.route('/', methods=['GET', 'POST'])
 def home_route():
     return jsonify({
@@ -48,30 +59,55 @@ def home_route():
         'developer': 'Cristian Valero Abundio'
     })
 
-@app.route('/register/', methods=['GET', 'POST'])
-def signup_user():  
-    data = request.get_json()  
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = Users(public_id=str(uuid.uuid4()), email=data['email'], password=hashed_password) 
-    db.session.add(new_user)  
-    db.session.commit()    
+@app.route('/register/', methods=['POST'])
+def signup_user():
+    if request.is_json():
+        data = request.get_json()  
+        hashed_password = generate_password_hash(data['password'], method='sha256')
+        new_user = Users(public_id=str(uuid.uuid4()), email=data['email'], password=hashed_password) 
+        db.session.add(new_user)  
+        db.session.commit()    
+        return jsonify({
+            'message': 'Registered successfully.'
+        }), 200
     return jsonify({
-        'message': 'Registered successfully.'
-    }), 200
+        'message': 'You must send json data only.'
+    }), 401
 
-@app.route('/login/', methods=['GET', 'POST'])  
+@app.route('/login/', methods=['POST'])  
 def login_user(): 
     auth = request.authorization   
     if not auth or not auth.username or not auth.password:  
         return make_response('Could not verify.', 401, {'WWW.Authentication': 'Basic realm: "Login required."'})    
-    user = Users.query.filter_by(email=auth.username).first()   
-    if check_password_hash(user.password, auth.password):  
-        token = jwt.encode({
-            'public_id': user.public_id,
-            'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-            }, app.config['SECRET_KEY']) 
-        return jsonify({'token' : token}) 
+    user = Users.query.filter_by(email=auth.username).first()
+    if user != None:
+        if check_password_hash(user.password, auth.password):  
+            token = jwt.encode({
+                'public_id': user.public_id,
+                'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+                }, app.config['SECRET_KEY']) 
+            return jsonify({'token' : token})
+    else:
+        return jsonify({
+            "message": "Requested user not found."
+        })
     return make_response('Could not verify',  401, {'WWW.Authentication': 'Basic realm: "Login required."'})
+
+@app.route('/agata/conversate/en/', methods=['POST'])
+@token_required
+def generate_english_answer(current_user):
+    if request.method == 'POST':
+        request_data = request.get_json() #{'id': 3, 'question': ''}
+        id, question = str(request_data['id']), str(request_data['question']) 
+        req = requests.post(url=API_OBTAIN_ANSWER, json={'id': id, 'question': question})
+        if req.status_code == 200:
+            answer = req.json()['answer']
+            return jsonify({
+                'question': str(question),
+                'answer': str(answer)
+            })
+        else:
+            throw_request_error()
 
 @app.errorhandler(404)
 def route_not_found(exc):
