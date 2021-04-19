f28767d2068bb522a8f4b2fc2570c4906cc82ccc
CristianValero <pauito03@hotmail.com>
2021-04-19 09:55:17 +0200

    functional jwt auth protocol

--- Bridge-API/apiservice2.py
+++ Bridge-API/apiservice2.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Th1s1ss3cr3t'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/authapi.db'.format(os.path.abspath(os.curdir))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/cristian/Desktop/AgataBot-v2/AgataBot/authapi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
@@ -41,7 +41,7 @@ def token_required(f):
     return decorator
 
 @app.route('/', methods=['GET', 'POST'])
-def home_route(request):
+def home_route():
     return jsonify({
         'message': 'Welcome to the Bridge API.',
         'info': 'This API intercommunicates all the rest of the microservices in the network to balance and distribute all the incoming requests.',
@@ -64,13 +64,13 @@ def login_user():
     auth = request.authorization   
     if not auth or not auth.username or not auth.password:  
         return make_response('Could not verify.', 401, {'WWW.Authentication': 'Basic realm: "Login required."'})    
-    user = Users.query.filter_by(email=auth.email).first()   
+    user = Users.query.filter_by(email=auth.username).first()   
     if check_password_hash(user.password, auth.password):  
         token = jwt.encode({
             'public_id': user.public_id,
             'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
             }, app.config['SECRET_KEY']) 
-        return jsonify({'token' : token.decode('UTF-8')}) 
+        return jsonify({'token' : token}) 
     return make_response('Could not verify',  401, {'WWW.Authentication': 'Basic realm: "Login required."'})
 
 @app.errorhandler(404)
@@ -78,7 +78,6 @@ def route_not_found(exc):
     return jsonify({
         'code': 404,
         'message': 'The route you requested not found. Please, try again or contact with an administrator.'
-    }), exc.status_code
-
+    })
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3005)
