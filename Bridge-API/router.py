37780a7e56316c59de3185892c84831373422632
CristianValero <pauito03@hotmail.com>
2021-04-20 11:44:04 +0200

    checkpoint

--- Bridge-API/router.py
+++ Bridge-API/router.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 from functools import wraps
 import jwt
 
-#
 class RouterManager():
 
     def __init__(self, name, app, db, users, api_adress_sources):
