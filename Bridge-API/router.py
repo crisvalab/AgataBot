6a73f1133e509128950fa3415a76aee6ac83fc0b
CristianValero <pauito03@hotmail.com>
2021-04-20 10:26:11 +0200

    updates

--- Bridge-API/router.py
+++ Bridge-API/router.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from functools import wraps
 import jwt
 
+#
 class RouterManager():
 
     def __init__(self, name, app, db, users, api_adress_sources):
@@ -15,23 +16,23 @@ class RouterManager():
     def config_routes(self) -> Blueprint:
         pass
 
+    ''' This function controll accesing routes from external IP adress.
+        External requests -> JWT AUTH -> GRANT ACCESS
+        Internal requests / Local requests -> GRANT ACCESS DIRECTLY '''
     def token_required(self, f):
         @wraps(f)
         def decorator(*args, **kwargs):
-            token = None
-            if 'x-access-tokens' in request.headers:
-                token = request.headers['x-access-tokens']
-            if not token:
-                return jsonify({
-                    'message': 'A valid token is missing.'
-                })
-            try:
-                data = jwt.decode(token, self.app.config['SECRET_KEY'], algorithms=["HS256"])
-                current_user = self.users.query.filter_by(public_id=data['public_id']).first()
-            except:
-                return jsonify({
-                    'message': 'Invalid token.'
-                })
-
-            return f(current_user, *args, **kwargs)
+            if request.remote_addr != '127.0.0.1':
+                if 'x-access-tokens' in request.headers:
+                    token = request.headers['x-access-tokens']
+                    try:
+                        data = jwt.decode(token, self.app.config['SECRET_KEY'], algorithms=["HS256"])
+                        current_user = self.users.query.filter_by(public_id=data['public_id']).first()
+                        return f(current_user, *args, **kwargs)
+                    except:
+                        return jsonify({ 'message': 'Invalid token.' })
+                else:
+                    return jsonify({ 'message': 'A valid token is missing.' })
+            else:
+                return f(None, *args, **kwargs)
         return decorator
