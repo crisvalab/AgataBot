63e19118e22ad4171603d4b5f88027e9bdc9f549
CristianValero <pauito03@hotmail.com>
2021-05-06 09:56:29 +0200

    implemented config.json for future escalability implementations

--- SentimentAnalysis-API/apiservice.py
+++ SentimentAnalysis-API/apiservice.py
@@ -1,5 +1,47 @@
-from analysis import SentimentAnalysis
+from analysis.analysis import SentimentAnalysis
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+@app.route('/', methods=['GET', 'POST', 'HEAD'])
+async def home_route(request):
+    return JSONResponse(content={
+        'message': 'Welcome to Translator EN-ES/ES-EN API.',
+        'info': 'This API only will run from 0.0.0.0 and is for local use only.',
+        'developer': 'Cristian Valero Abundio'
+    }, headers=response_header)
+
+@app.route('/translator/es/', methods=['POST'])
+async def translate_to_es(request):
+    if request.method == 'POST':
+        request_data = await request.json() #{'sentence': 'Hy! My name is Cristian.'}
+        sentence = str(request_data['sentence'])
+        translation = translator.translate_to_spanish(sentence)
+        return JSONResponse(content={
+            'sentence': sentence,
+            'translation': str(translation)
+        }, headers=response_header)
+
+@app.route('/translator/en/', methods=['POST'])
+async def translate_to_en(request):
+    if request.method == 'POST':
+        request_data = await request.json() #{'sentence': 'Hola, me llamo Paco.'}
+        sentence = str(request_data['sentence'])
+        translation = translator.translate_to_english(sentence)
+        return JSONResponse(content={
+            'sentence': sentence,
+            'translation': translation
+        }, headers=response_header)
+
+@app.exception_handler(404)
+def route_not_found(request, exc):
+    return JSONResponse(content={
+        'code': 404,
+        'message': 'The route you requested not found. Please, try again or contact with an administrator.'
+    }, status_code=exc.status_code)
 
 if __name__ == '__main__':
     an = SentimentAnalysis()
-    print(an.analize("I don't like pizza."))
+    uvicorn.run(app, host='0.0.0.0', port=3003)
+    #print(an.analize("I don't like pizza."))
