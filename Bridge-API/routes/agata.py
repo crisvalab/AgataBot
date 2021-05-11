a65f5fe4c65fc3d04df08cbc3adfe047233e5b6e
CristianValero <pauito03@hotmail.com>
2021-05-11 10:35:06 +0200

    updated bridge/gateway security for not empty json requests

--- Bridge-API/routes/agata.py
+++ Bridge-API/routes/agata.py
@@ -23,50 +23,56 @@ class AgataRouter(RouterManager):
         @self.token_required
         def generate_english_answer(current_user):
             if request.method == 'POST':
-                request_data = request.get_json() #{'id': 3, 'question': ''}
-                id, question = str(request_data['id']), str(request_data['question'])
-                url = self.addresses['agata']['answer']
-                req = requests.post(url=url, json={'id': id, 'question': question})
-                if req.status_code == 200:
-                    answer = req.json()['answer']
-                    return jsonify({
-                        'question': str(question),
-                        'answer': str(answer)
-                    })
+                try:
+                    request_data = request.get_json() #{'id': 3, 'question': ''}
+                    id, question = str(request_data['id']), str(request_data['question'])
+                    url = self.addresses['agata']['answer']
+                    req = requests.post(url=url, json={'id': id, 'question': question})
+                    if req.status_code == 200:
+                        answer = req.json()['answer']
+                        return jsonify({
+                            'question': str(question),
+                            'answer': str(answer)
+                        })
+                except:
+                    return jsonify({'message:': 'Request not valid.'})
             return jsonify({ 'message': 'Request not valid or internal error. Please contact to the administrator.' })
 
         @self.blueprint.route('/agata/conversate/es/', methods=['POST'])
         @self.token_required
         def generate_spanish_answer(current_user):
             if request.method == 'POST':
-                request_data = request.get_json() #{'id': '3', 'question': 'Hola, ¿como te llamas?'}
-                id, question = str(request_data['id']), str(request_data['question'])
-                url = self.addresses['translator']['en']
-                req = requests.post(url=url, json={'sentence': question})
-                if req.status_code == 200:
-                    sentence_to_en = req.json()
-                    sentence_to_en = sentence_to_en['translation'] #This is the translated sentence.
-                    url = self.addresses['agata']['answer']
-                    req = requests.post(url=url, json={'id': id, 'question': sentence_to_en})
+                try:
+                    request_data = request.get_json() #{'id': '3', 'question': 'Hola, ¿como te llamas?'}
+                    id, question = str(request_data['id']), str(request_data['question'])
+                    url = self.addresses['translator']['en']
+                    req = requests.post(url=url, json={'sentence': question})
                     if req.status_code == 200:
-                        answer = req.json()
-                        answer = answer['answer'] #This is the retrived answer.
-                        url = self.addresses['translator']['es']
-                        req = requests.post(url=url, json={'sentence': answer})
+                        sentence_to_en = req.json()
+                        sentence_to_en = sentence_to_en['translation'] #This is the translated sentence.
+                        url = self.addresses['agata']['answer']
+                        req = requests.post(url=url, json={'id': id, 'question': sentence_to_en})
                         if req.status_code == 200:
-                            sentence_to_es = req.json()
-                            sentence_to_es = sentence_to_es['translation']
-                            return jsonify({
-                                'question': str(question),
-                                'question_to_en': str(sentence_to_en),
-                                'answer': str(answer),
-                                'answer_to_es': str(sentence_to_es)
-                            }), 200
+                            answer = req.json()
+                            answer = answer['answer'] #This is the retrived answer.
+                            url = self.addresses['translator']['es']
+                            req = requests.post(url=url, json={'sentence': answer})
+                            if req.status_code == 200:
+                                sentence_to_es = req.json()
+                                sentence_to_es = sentence_to_es['translation']
+                                return jsonify({
+                                    'question': str(question),
+                                    'question_to_en': str(sentence_to_en),
+                                    'answer': str(answer),
+                                    'answer_to_es': str(sentence_to_es)
+                                }), 200
+                            else:
+                                return jsonify({ 'message': 'Request not valid or internal error. Please contact to the administrator.' })
                         else:
                             return jsonify({ 'message': 'Request not valid or internal error. Please contact to the administrator.' })
                     else:
                         return jsonify({ 'message': 'Request not valid or internal error. Please contact to the administrator.' })
-                else:
-                    return jsonify({ 'message': 'Request not valid or internal error. Please contact to the administrator.' })
+                except:
+                    return jsonify({'message:': 'Request not valid.'})
 
         return self.blueprint
