0c4dd88e5848cd6bb20d35d85a04034a06130962
CristianValero <pauito03@hotmail.com>
2021-05-11 10:29:48 +0200

    SentimentAnalysis API mode working

--- SentimentAnalysis-API/apiservice.py
+++ SentimentAnalysis-API/apiservice.py
@@ -7,7 +7,7 @@ from starlette.routing import Route
 app = Starlette(debug=False)
 response_header = { 'Access-Control-Allow-Origin': '*' }
 
-an = SentimentAnalysis()
+analyzer = SentimentAnalysis()
 
 @app.route('/', methods=['GET', 'POST', 'HEAD'])
 async def home_route(request):
@@ -24,6 +24,23 @@ def route_not_found(request, exc):
         'message': 'The route you requested not found. Please, try again or contact with an administrator.'
     }, status_code=exc.status_code)
 
+
+@app.route('/analyze/', methods=['POST'])
+async def generate_answer(request):
+    if request.method == 'POST':
+        try:
+            request_data = await request.json() #{ 'text': 'This is the text to analize' }
+            if request_data != None:
+                text = str(request_data['text'])
+                analysis_result = analyzer.analize(text)
+                return JSONResponse(content={
+                    'text': str(text),
+                    'result': analysis_result,
+                }, headers=response_header)
+            else:
+                return JSONResponse(content={'error:': 'You should send json body.'}, headers=response_header)
+        except:
+            return JSONResponse(content={'error:': 'Request not valid.'}, headers=response_header)
+
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=3003)
-    #print(an.analize("I don't like pizza."))
